82574ec7c27aa62049c73782201a28eca55e573d
Zachar1er <zachary.barre0809@gmail.com>
2025-04-01 15:53:42 +0200

    Ajout des animations de déplacement

--- chess_game/lib/widgets/chess_piece.dart
+++ chess_game/lib/widgets/chess_piece.dart
@@ -13,8 +13,15 @@ class ChessPieceWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     // For initial testing, use Unicode chess symbols
     String symbol = '';
+=======
+    //Creation du path vers la texture de la piece
+    String pieceColor = piece.color == PieceColor.white ? 'w' : 'b';
+    String pieceType = '';
+    
+>>>>>>> Stashed changes
     switch (piece.type) {
       case PieceType.king:
         symbol = piece.color == PieceColor.white ? '♔' : '♚';
@@ -35,6 +42,7 @@ class ChessPieceWidget extends StatelessWidget {
         symbol = piece.color == PieceColor.white ? '♙' : '♟';
         break;
     }
+<<<<<<< Updated upstream
 
     return SizedBox(
       width: size,
@@ -54,6 +62,29 @@ class ChessPieceWidget extends StatelessWidget {
             ],
           ),
         ),
+=======
+    
+    //Chemin complet
+    String imagePath = 'assets/images/pieces/${pieceType}.${pieceColor}.png';
+    
+    return SizedBox(
+      width: size,
+      height: size,
+      child: Image.asset(
+        imagePath,
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
+        errorBuilder: (context, error, stackTrace) {
+          //Imafe debug, insipiré du debug model de Source
+          return Image.asset(
+            'assets/images/pieces/default.png',
+            width: size,
+            height: size,
+            fit: BoxFit.contain,
+          );
+        },
+>>>>>>> Stashed changes
       ),
     );
 
